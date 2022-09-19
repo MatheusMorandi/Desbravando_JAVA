2caa4ebf7aaba854ab1a33e3ded7721f3ba8bb71
MatheusMorandi <theu.morandi@gmail.com>
2022-09-19 11:24:41 +0000

    New

---
+++ Learning/Pokemon.java
@@ -0,0 +1,18 @@
+package Learning;
+
+public class Pokemon {
+
+    //Atributos
+    String tipo;
+    String nome;
+    
+    //Métodos
+
+    void pk (){
+
+        System.out.println("Type: " + this.tipo);
+        System.out.println("Name: " + this.nome);
+        }
+
+    
+}
---
+++ Learning/PokemonTest.java
@@ -0,0 +1,15 @@
+package Learning;
+public class PokemonTest{
+
+    public static void main (String args[]){
+
+        Pokemon poke = new Pokemon();
+
+        poke.nome = "Charmander";
+        poke.tipo = "Fogo";
+
+        poke.pk();
+
+    }
+    
+}
