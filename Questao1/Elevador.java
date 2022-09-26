825cf7800d25bbe2dae71591217e604de0dbc2cb
MatheusMorandi <theu.morandi@gmail.com>
2022-09-26 16:34:36 +0000

    New

--- Questao1/Elevador.java
+++ Questao1/Elevador.java
@@ -12,7 +12,7 @@ public class Elevador{
     void entra(){
         if (this.qntPessoa < this.capacidade ){
             this.qntPessoa += 1;
-            System.out.println("De "+(this.qntPessoa-1)+" pessoas foi para "+this.qntPessoa);
+            System.out.println("De "+(this.qntPessoa - 1)+" pessoas foi para "+ this.qntPessoa);
         }
         else{
             System.out.println("Não foi possível acrescentar uma pessoa, pois ja está lotado.");
@@ -22,7 +22,7 @@ public class Elevador{
     void sai (){
         if(this.qntPessoa > 0){
             this.qntPessoa -= 1;
-            System.out.println("De "+(this.qntPessoa+1)+" pessoas foi para "+this.qntPessoa);
+            System.out.println("De "+(this.qntPessoa + 1)+" pessoas foi para "+ this.qntPessoa);
         }
         else{
             System.out.println("Não foi possível sair ninguém, pois está vazio.");
@@ -32,7 +32,7 @@ public class Elevador{
     void sobe(){
         if (this.andarAtual < this.totalAndar){
             this.andarAtual += 1;
-            System.out.println("Fomos do andar "+(this.andarAtual-1)+" para o andar "+this.andarAtual);
+            System.out.println("Fomos do andar "+(this.andarAtual - 1)+" para o andar "+ this.andarAtual);
         }
         else{
             System.out.println("Não foi possível subir o andar pois ja estamos no último andar.");
@@ -42,7 +42,7 @@ public class Elevador{
     void desce(){
         if (this.andarAtual > 0){
             this.andarAtual -=1;
-            System.out.println("Fomos do andar "+(this.andarAtual+1)+" para o andar "+this.andarAtual);
+            System.out.println("Fomos do andar "+(this.andarAtual + 1)+" para o andar "+ this.andarAtual);
         }
         else{
             System.out.println("Não foi possível descer o andar pois ja estamos no térreo.");
